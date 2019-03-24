72758f8006198ad306d30d9940a43969c866bfd9
Chau <chau@simicart.com>
2019-03-24 09:00:39 +0700

    add IOS

---
+++ ios/PayFortSDK.framework/Versions/A/Headers/PayFortView.h
@@ -0,0 +1,69 @@
+//
+//  PayFortView.h
+//  PayFortProject
+//
+//  Created by PayFort on 4/15/16.
+//  Copyright © 2016 PayFort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class PFKJVFloatLabeledTextField;
+@interface PayFortView : UIView<UITextFieldDelegate>
+
+- (void)start;
+
+- (void)setupLanguage:(NSString*)lang;
+
+- (instancetype)initWitNibName:(NSString *)nibName inBundle:(NSBundle*)bundle;
+
+@property (assign, nonatomic)  BOOL IsAutoCompleteMode;
+
+@property (assign, nonatomic)  BOOL isPaymentOption;
+
+@property (assign, nonatomic)  BOOL isHideRemberMe;
+
+@property (assign, nonatomic)  BOOL isCustomNib;
+
+@property (assign, nonatomic)  BOOL isNPSCard;
+
+
+@property (assign, nonatomic)  int CurrentTagTextField;
+
+@property (weak, nonatomic) IBOutlet UIView *titleView;
+@property (weak, nonatomic) IBOutlet UILabel *yourReceiptLbl;
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLbl;
+
+@property (weak, nonatomic) IBOutlet UIButton *BackBtn;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *headerHeight;
+
+@property (weak, nonatomic) IBOutlet UILabel *PriceLbl;
+@property (weak, nonatomic) IBOutlet PFKJVFloatLabeledTextField *CardNameTxt;
+@property (weak, nonatomic) IBOutlet PFKJVFloatLabeledTextField *CardNumberTxt;
+
+
+@property (weak, nonatomic) IBOutlet PFKJVFloatLabeledTextField *CVCNumberTxt;
+
+@property (weak, nonatomic) IBOutlet PFKJVFloatLabeledTextField *ExpDateTxt;
+
+@property (weak, nonatomic) IBOutlet UILabel *cardNumberErrorlbl;
+@property (weak, nonatomic) IBOutlet UILabel *cVCNumberErrorlbl;
+@property (weak, nonatomic) IBOutlet UILabel *expDateErrorlbl;
+
+@property (weak, nonatomic) IBOutlet UISwitch *savedCardSwitch;
+
+@property (weak, nonatomic) IBOutlet UIButton *paymentBtn;
+
+@property (weak, nonatomic) IBOutlet UILabel *saveCardLbl;
+@property (weak, nonatomic) IBOutlet UIImageView *imageCard;
+
+
+
+@property (weak, nonatomic) IBOutlet UIView *SaveCardView;
+
+
+
+
+
+@end
--- ios/RNReactNativePayfortSdk.m
+++ ios/RNReactNativePayfortSdk.m
@@ -1,13 +1,59 @@
 
 #import "RNReactNativePayfortSdk.h"
 
-@implementation RNReactNativePayfortSdk
+@implementation RNReactNativePayfortSdk{
+    RCTResponseSenderBlock onDoneClick;
+    RCTResponseSenderBlock onCancelClick;
+}
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(openPayfort:(NSDictionary *)indic createDialog:(RCTResponseSenderBlock)doneCallback createDialog:(RCTResponseSenderBlock)cancelCallback){
+    onDoneClick = doneCallback;
+    onCancelClick = cancelCallback;
+    
+    PayFortController *payfort = [[PayFortController alloc] initWithEnviroment:KPayFortEnviromentSandBox];
+    payfort.IsShowResponsePage = true;
+    NSLog(@"%@", [payfort getUDID]);
+    NSMutableDictionary *request = [[NSMutableDictionary alloc]init];
+    [request setValue:indic[@"amount"] forKey:@"amount"];
+    [request setValue:indic[@"currency"] forKey:@"currency"];
+    [request setValue:indic[@"customer_email"] forKey:@"customer_email"];
+    [request setValue:indic[@"customer_name"] forKey:@"customer_name"];
+    [request setValue:indic[@"customer_ip"] forKey:@"customer_ip"];
+    [request setValue:indic[@"merchant_reference"] forKey:@"merchant_reference"];
+    [request setValue:indic[@"sdk_token"] forKey:@"sdk_token"];
+    [request setValue:@"PURCHASE" forKey:@"command"];
+    [request setValue:@"en" forKey:@"language"];
+    [request setValue:@"VISA" forKey:@"payment_option"];
+    [request setValue:@"ECOMMERCE" forKey:@"eci"];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+        NSArray *events = @[];
+        [payfort callPayFortWithRequest:request currentViewController:rootViewController
+                                Success:^(NSDictionary *requestDic, NSDictionary *responeDic) {
+                                    NSLog(@"Success");
+                                    NSLog(@"responeDic=%@",responeDic);
+                                    onDoneClick(@[[NSNull null], events]);
+                                }
+                               Canceled:^(NSDictionary *requestDic, NSDictionary *responeDic) {
+                                   NSLog(@"Canceled");
+                                   NSLog(@"responeDic=%@",responeDic);
+                                   onCancelClick(@[[NSNull null], events]);
+                               }
+                                  Faild:^(NSDictionary *requestDic, NSDictionary *responeDic, NSString *message) {
+                                      NSLog(@"Faild");
+                                      NSLog(@"responeDic=%@",responeDic);
+                                  }];
+        
+    });
+}
+
 
 @end
   
