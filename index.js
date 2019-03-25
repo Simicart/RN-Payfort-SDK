890247a2148441098c8706a70bd077a9c1cebe69
ThanhQuang1410 <ttquang1410@gmail.com>
2019-03-25 16:37:49 +0700

    commit file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { NativeModules } from 'react-native';
 
-const { RNReactNativePayfortSdk } = NativeModules;
+const { RNReactNativePayfortSdk } = NativeModules;;
 
 export default RNReactNativePayfortSdk;
