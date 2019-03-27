ec57b1622d759bbe5d1fd1a1da50f07807c3da45
chau <chau@simicart.com>
2019-03-27 21:11:35 +0700

    update fix bug ios

--- ios/RNReactNativePayfortSdk.m
+++ ios/RNReactNativePayfortSdk.m
@@ -9,6 +9,7 @@
     PayFortController *payfort;
     UIViewController *rootViewController;
     UIActivityIndicatorView *simiLoading;
+    NSString *udidString;
 
 }
 
@@ -19,139 +20,153 @@
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(openPayfort:(NSDictionary *)indic createDialog:(RCTResponseSenderBlock)doneCallback createDialog:(RCTResponseSenderBlock)cancelCallback){
-    onDoneClick = doneCallback;
-    onCancelClick = cancelCallback;
+  onDoneClick = doneCallback;
+  onCancelClick = cancelCallback;
+  
+  udidString = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+  
+  dispatch_async(dispatch_get_main_queue(), ^{
     data = [[NSDictionary alloc] initWithDictionary:indic];
-    
+    rootViewController = (UIViewController*)[UIApplication sharedApplication].delegate.window.rootViewController;
     [self handleTokenString];
+  });
+  
+  
 }
 
 - (void)handleTokenString{
   [self startLoadingData];
-    dispatch_async(dispatch_get_main_queue(), ^{
-        rootViewController = (UIViewController*)[UIApplication sharedApplication].delegate.window.rootViewController;
-    });
-    
-    NSMutableString *post = [NSMutableString string];
-    [post appendString:data[@"request_phrase"]];
-    [post appendFormat:@"access_code=%@", data[@"access_code"]];
-    [post appendFormat:@"device_id=%@",  [payfort getUDID ]];
-    [post appendFormat:@"language=%@", @"en"];
-    [post appendFormat:@"merchant_identifier=%@", data[@"merchant_identifier"]];
-    [post appendFormat:@"service_command=%@", @"SDK_TOKEN"];
-    [post appendString:data[@"request_phrase"]];
-    
-    [self requestTokenSDK:[self sha1Encode:post]];
+  
+  NSMutableString *post = [NSMutableString string];
+  [post appendString:data[@"request_phrase"]];
+  [post appendFormat:@"access_code=%@", data[@"access_code"]];
+  [post appendFormat:@"device_id=%@",  udidString];
+  [post appendFormat:@"language=%@", @"en"];
+  [post appendFormat:@"merchant_identifier=%@", data[@"merchant_identifier"]];
+  [post appendFormat:@"service_command=%@", @"SDK_TOKEN"];
+  [post appendString:data[@"request_phrase"]];
+  
+  [self requestTokenSDK:[self sha1Encode:post]];
 }
 
 - (void)requestTokenSDK:(NSString *)signature{
-    NSError *error;
-    
-    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
-    NSURL *url;
+  NSError *error;
+  NSURL *url;
+  if ([data[@"is_live"] isEqualToString:@"1"]) {
+    url = [NSURL URLWithString:@"https://paymentservices.payfort.com/FortAPI/paymentApi"];
+  }else{
+    url = [NSURL URLWithString:@"https://sbpaymentservices.payfort.com/FortAPI/paymentApi"];
+  }
   
-    if ([data[@"is_live"] isEqualToString:@"1"]) {
-      url = [NSURL URLWithString:@"https://paymentservices.payfort.com/FortAPI/paymentApi"];
-    }else{
-      url = [NSURL URLWithString:@"https://sbpaymentservices.payfort.com/FortAPI/paymentApi"];
-    }
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url
-                                                           cachePolicy:NSURLRequestUseProtocolCachePolicy
-                                                       timeoutInterval:60.0];
-    
-    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request addValue:@"application/json" forHTTPHeaderField:@"Accept"];
-    
-    [request setHTTPMethod:@"POST"];
-    NSDictionary *mapData = [[NSDictionary alloc] initWithObjectsAndKeys:
-                             @"SDK_TOKEN", @"service_command",
-                             data[@"access_code"], @"access_code",
-                             data[@"merchant_identifier"], @"merchant_identifier",
-                             @"en", @"language",
-                             [payfort getUDID], @"device_id",
-                             signature, @"signature",
-                             
-                             nil];
-    NSData *postData = [NSJSONSerialization dataWithJSONObject:mapData options:0 error:&error];
-    [request setHTTPBody:postData];
-    
-    NSURLSessionDataTask *postDataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        if (error) {
-            
-        } else {
-            NSError *parseError = nil;
-            NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
-            [self openPayfort:responseDictionary[@"sdk_token"]];
-        }
-    }];
-    [postDataTask resume];
+  NSDictionary* tmp = @{ @"service_command": @"SDK_TOKEN",
+                         @"merchant_identifier": data[@"merchant_identifier"],
+                         @"access_code": data[@"access_code"],
+                         @"signature": signature,
+                         @"language": @"en",
+                         @"device_id": udidString
+                         };
+  NSData *postdata = [NSJSONSerialization dataWithJSONObject:tmp options:0 error:&error];
+  
+  NSString *postLength = [NSString stringWithFormat:@"%ld",[postdata length]];
+  NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+  [request setURL:url];
+  [request setHTTPMethod:@"POST"];
+  [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+  [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+  [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+  [request setHTTPBody:postdata];
+  
+  NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request
+                                                               completionHandler:^(NSData *data,
+                                                                                   NSURLResponse *response,
+                                                                                   NSError *error)
+                                {
+                                  if (!error)
+                                  {
+                                    NSError *error = nil;
+                                    id object = [NSJSONSerialization
+                                                 JSONObjectWithData:data
+                                                 options:0
+                                                 error:&error];
+                                    NSLog(@"object %@",object);
+                                    
+                                    if(error) {
+                                      NSLog(@"Error %@",error);
+                                      return;
+                                    }
+                                    NSString *sdk_token = object[@"sdk_token"];
+                                    [self openPayfort:sdk_token];
+                                    
+                                  }
+                                  else
+                                  {
+                                    NSLog(@"Error: %@", error.localizedDescription);
+                                  }
+                                }];
+  [task resume];
 }
 
 - (void)openPayfort:(NSString *)sdkToken{
-    [self stopLoadingData];
-
-    NSMutableDictionary *request = [[NSMutableDictionary alloc]init];
-    [request setValue:data[@"amount"] forKey:@"amount"];
-    [request setValue:data[@"currency"] forKey:@"currency"];
-    [request setValue:data[@"customer_email"] forKey:@"customer_email"];
-    [request setValue:data[@"customer_name"] forKey:@"customer_name"];
-    [request setValue:data[@"customer_ip"] forKey:@"customer_ip"];
-    [request setValue:data[@"merchant_reference"] forKey:@"merchant_reference"];
-    [request setValue:sdkToken forKey:@"sdk_token"];
-    [request setValue:@"PURCHASE" forKey:@"command"];
-    [request setValue:@"en" forKey:@"language"];
-    [request setValue:@"VISA" forKey:@"payment_option"];
-    [request setValue:@"ECOMMERCE" forKey:@"eci"];
+  [self stopLoadingData];
+  NSMutableDictionary *request = [[NSMutableDictionary alloc]init];
+  [request setValue:data[@"amount"] forKey:@"amount"];
+  [request setValue:data[@"currency"] forKey:@"currency"];
+  [request setValue:data[@"customer_email"] forKey:@"customer_email"];
+  [request setValue:data[@"customer_name"] forKey:@"customer_name"];
+  [request setValue:data[@"customer_ip"] forKey:@"customer_ip"];
+  [request setValue:data[@"merchant_reference"] forKey:@"merchant_reference"];
+  [request setValue:sdkToken forKey:@"sdk_token"];
+  [request setValue:@"PURCHASE" forKey:@"command"];
+  [request setValue:@"en" forKey:@"language"];
+  [request setValue:@"VISA" forKey:@"payment_option"];
+  [request setValue:@"ECOMMERCE" forKey:@"eci"];
   
   if ([data[@"is_live"] isEqualToString:@"1"]) {
     payfort = [[PayFortController alloc] initWithEnviroment:KPayFortEnviromentProduction];
   }else{
     payfort = [[PayFortController alloc] initWithEnviroment:KPayFortEnviromentSandBox];
   }
+  payfort.IsShowResponsePage = true;
   
-    payfort.IsShowResponsePage = true;
-    
-    NSArray *events = @[];
-    [payfort callPayFortWithRequest:request currentViewController:rootViewController
-                            Success:^(NSDictionary *requestDic, NSDictionary *responeDic) {
-                                NSLog(@"Success");
-                                NSLog(@"responeDic=%@",responeDic);
-                                onDoneClick(@[[NSNull null], events]);
-                            }
-                           Canceled:^(NSDictionary *requestDic, NSDictionary *responeDic) {
-                               NSLog(@"Canceled");
-                               NSLog(@"responeDic=%@",responeDic);
-                               onCancelClick(@[[NSNull null], events]);
-                           }
-                              Faild:^(NSDictionary *requestDic, NSDictionary *responeDic, NSString *message) {
-                                  NSLog(@"Faild");
-                                  NSLog(@"responeDic=%@",responeDic);
-                              }];
+  NSArray *events = @[];
+  [payfort callPayFortWithRequest:request currentViewController:rootViewController
+                          Success:^(NSDictionary *requestDic, NSDictionary *responeDic) {
+                            NSLog(@"Success");
+                            NSLog(@"responeDic=%@",responeDic);
+                            onDoneClick(@[[NSNull null], events]);
+                          }
+                         Canceled:^(NSDictionary *requestDic, NSDictionary *responeDic) {
+                           NSLog(@"Canceled");
+                           NSLog(@"responeDic=%@",responeDic);
+                           onCancelClick(@[[NSNull null], events]);
+                         }
+                            Faild:^(NSDictionary *requestDic, NSDictionary *responeDic, NSString *message) {
+                              NSLog(@"Faild");
+                              NSLog(@"responeDic=%@",responeDic);
+                            }];
 }
 
 
 - (NSString*)sha1Encode:(NSString*)input {
-    const char *cstr = [input cStringUsingEncoding:NSUTF8StringEncoding];
-    NSData *data = [NSData dataWithBytes:cstr length:input.length];
-    
-    uint8_t digest[CC_SHA256_DIGEST_LENGTH];
-    
-    CC_SHA256(data.bytes, (int)data.length, digest);
-    
-    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH * 2];
-    
-    for(int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++)
+  const char *cstr = [input cStringUsingEncoding:NSUTF8StringEncoding];
+  NSData *data = [NSData dataWithBytes:cstr length:input.length];
+  
+  uint8_t digest[CC_SHA256_DIGEST_LENGTH];
+  
+  CC_SHA256(data.bytes, (int)data.length, digest);
+  
+  NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH * 2];
+  
+  for(int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++)
     [output appendFormat:@"%02x", digest[i]];
-    
-    return output;
+  
+  return output;
 }
-
 
 - (void)startLoadingData{
   dispatch_async(dispatch_get_main_queue(), ^{
     CGRect frame = rootViewController.view.bounds;
-    simiLoading = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    simiLoading = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
     simiLoading.hidesWhenStopped = YES;
     simiLoading.center = CGPointMake(frame.size.width/2, frame.size.height/2);
     [rootViewController.view addSubview:simiLoading];
