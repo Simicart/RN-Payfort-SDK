5337bdb56c87e79086badc6a5cc464cb94ef6e0f
Glenn <glenn@simicart.com>
2019-07-26 10:57:32 +0700

    Remove request sdk token

--- android/src/main/java/com/reactlibrary/PayfortActivity.java
+++ android/src/main/java/com/reactlibrary/PayfortActivity.java
@@ -1,32 +1,15 @@
 package com.reactlibrary;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.payfort.fort.android.sdk.base.FortSdk;
 import com.payfort.fort.android.sdk.base.callbacks.FortCallBackManager;
 import com.payfort.fort.android.sdk.base.callbacks.FortCallback;
 import com.payfort.sdk.android.dependancies.base.FortInterfaces;
 import com.payfort.sdk.android.dependancies.models.FortRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -37,9 +20,9 @@ import java.util.Map;
 public class PayfortActivity extends Activity {
 
     FortCallBackManager fortCallback;
-    ProgressDialog pbLoading;
     String deviceId, isLive, accessCode, merchantIdentifier, requestPhrase,
-            customerEmail, currency, amount, merchantReference, customerName, customerIp, paymentOption, orderDescription;
+            customerEmail, currency, amount, merchantReference, customerName, customerIp, paymentOption, orderDescription,
+            responsePhrase, sdkToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +31,10 @@ public class PayfortActivity extends Activity {
 
         parseData();
 
-        pbLoading = new ProgressDialog(this);
-        pbLoading.setMessage("Fetching data...");
-        pbLoading.show();
-
         fortCallback = FortCallback.Factory.create();
         deviceId = FortSdk.getDeviceId(PayfortActivity.this);
 
-        requestGetToken();
+        startFortRequest(sdkToken);
     }
 
     private void parseData() {
@@ -71,6 +50,9 @@ public class PayfortActivity extends Activity {
         }
         if (intent.hasExtra("request_phrase")) {
             requestPhrase = intent.getStringExtra("request_phrase");
+        }
+        if (intent.hasExtra("response_phrase")) {
+            responsePhrase = intent.getStringExtra("response_phrase");
         }
         if (intent.hasExtra("customer_email")) {
             customerEmail = intent.getStringExtra("customer_email");
@@ -96,69 +78,9 @@ public class PayfortActivity extends Activity {
         if (intent.hasExtra("order_description")) {
             orderDescription = intent.getStringExtra("order_description");
         }
-    }
-
-    private void requestGetToken() {
-        String url = "https://sbpaymentservices.payfort.com/FortAPI/paymentApi";
-        if(isLive.equals("1")) {
-            url = "https://paymentservices.payfort.com/FortAPI/paymentApi";
+        if (intent.hasExtra("sdk_token")) {
+            sdkToken = intent.getStringExtra("sdk_token");
         }
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-        Map<String, String> params = new HashMap<>();
-        params.put("service_command", "SDK_TOKEN");
-        params.put("access_code", accessCode);
-        params.put("merchant_identifier", merchantIdentifier);
-        params.put("language", "en");
-        params.put("device_id", deviceId);
-        params.put("signature", hashSignature(requestPhrase + "access_code=" + accessCode + "device_id=" + deviceId + "language=enmerchant_identifier=" + merchantIdentifier + "service_command=SDK_TOKEN" + requestPhrase));
-        JSONObject parameters = new JSONObject(params);
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                pbLoading.dismiss();
-                Log.e("SUCCESS", response.toString());
-                try {
-                    startFortRequest(response.getString("sdk_token"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    finish();
-                    RNPayfortSdkModule.onFail.invoke("Error while fetching data");
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-                pbLoading.dismiss();
-                Log.e("FAIL", error.toString());
-                finish();
-                RNPayfortSdkModule.onFail.invoke();
-            }
-        });
-        queue.add(jsonRequest);
-    }
-
-    private String hashSignature(String originalString) {
-        MessageDigest digest = null;
-        try {
-            digest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        byte[] encodedhash = digest.digest(
-                originalString.getBytes(StandardCharsets.UTF_8));
-        return bytesToHex(encodedhash);
-    }
-
-    private String bytesToHex(byte[] hash) {
-        StringBuffer hexString = new StringBuffer();
-        for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
-            if (hex.length() == 1) hexString.append('0');
-            hexString.append(hex);
-        }
-        return hexString.toString();
     }
 
     private void startFortRequest(String sdkToken) {
